24f20b0d286e6262bae88a34c993893bbf4446ac
Ewere-charles <ewerecharles665@gmail.com>
2024-11-01 06:29:58 +0100

    just work on cors

--- index.js
+++ index.js
@@ -27,6 +27,9 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 }));
+
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(morgan('dev')); // Logging middleware
 
